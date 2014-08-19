1f753ae6c9419adbd5949e30306214c422db71aa
Ronald Martin <ronaldsmartin@gmail.com>
2014-08-18 22:19:34 -0700

    Hook up buttons in Contact view

--- app/src/main/java/org/mdssf/materdolorosa/fragment/ParishContactFragment.java
+++ app/src/main/java/org/mdssf/materdolorosa/fragment/ParishContactFragment.java
@@ -1,11 +1,13 @@
 package org.mdssf.materdolorosa.fragment;
 
-import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.mdssf.materdolorosa.R;
@@ -13,6 +15,10 @@ import org.mdssf.materdolorosa.R;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.TimeZone;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 
 /**
@@ -24,12 +30,20 @@ public class ParishContactFragment extends Fragment {
     @SuppressWarnings("unused")
     public static final String TAG = "ParishContactFragment";
 
+    //region Views
+    @InjectView(R.id.card_parish_currently_open)
+    TextView mParishOfficeOpenIndicator;
+    @InjectView(R.id.card_ccf_currently_open)
+    TextView mCcfOfficeOpenIndicator;
+    //endregion
+
+    //region Fragment Lifecycle
     public ParishContactFragment() {
         // Required empty public constructor
     }
 
     //region Office open check
-    private static boolean isParishOfficeOpen(String timezoneId) {
+    public static boolean isParishOfficeOpen(String timezoneId) {
         Date currentTime = Calendar.getInstance().getTime();
 
         // Return false if it's the weekend
@@ -57,8 +71,9 @@ public class ParishContactFragment extends Fragment {
         lunchHasStarted &= currentTime.before(timeMarker.getTime());
         return !lunchHasStarted;
     }
+    //endregion
 
-    private static boolean isCcfOfficeOpen(String timezoneId) {
+    public static boolean isCcfOfficeOpen(String timezoneId) {
         Date currentTime = Calendar.getInstance().getTime();
         boolean isOpen;
 
@@ -108,46 +123,119 @@ public class ParishContactFragment extends Fragment {
         return isOpen;
     }
 
-    //region Fragment Lifecycle
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-    //endregion
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_parish_contact, container, false);
 
-        // Set the office status text to Open if the user's current time is within the bounds of
-        // the office hours in the office's timezone.
-        final String timezoneId = getString(R.string.timezone_id);
-        if (isParishOfficeOpen(timezoneId))
-            setStatusOpen(rootView, R.id.card_parish_currently_open);
-        if (isCcfOfficeOpen(timezoneId)) setStatusOpen(rootView, R.id.card_ccf_currently_open);
+        ButterKnife.inject(this, rootView);
+
+        refreshOpenLabels();
 
         return rootView;
     }
 
     @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
+    public void onResume() {
+        super.onResume();
+        refreshOpenLabels();
+    }
+    //endregion
+
+    //region Open indicators
+    /**
+     * Set the office status text to Open if the user's current time is within the bounds of
+     * the office hours in the office's timezone.
+     */
+    private void refreshOpenLabels() {
+        final String timezoneId = getString(R.string.timezone_id);
+
+        if (isParishOfficeOpen(timezoneId)) setStatusOpen(mParishOfficeOpenIndicator);
+        else setStatusClosed(mParishOfficeOpenIndicator);
+
+        if (isCcfOfficeOpen(timezoneId)) setStatusOpen(mCcfOfficeOpenIndicator);
+        else setStatusClosed(mCcfOfficeOpenIndicator);
     }
 
     /**
-     * Set the TextView with {@code textViewId} in {@code rootView} to display the text
-     * 'Currently open' in light green.
+     * Set a TextView to display 'Currently closed' in red text.
      *
-     * @param rootView   Layout in which the TextView resides
-     * @param textViewId Id of the TextView to alter
+     * @param openIndicator The TextView to set to closed.
      */
-    private void setStatusOpen(View rootView, int textViewId) {
-        TextView officeStatus =
-                (TextView) rootView.findViewById(textViewId);
-        officeStatus.setText(R.string.currently_open);
-        officeStatus.setTextColor(getResources().getColor(R.color.holo_green_light));
+    private void setStatusClosed(TextView openIndicator) {
+        openIndicator.setText(R.string.currently_closed);
+        openIndicator.setTextColor(getResources().getColor(R.color.holo_red_dark));
+    }
+
+    /**
+     * Set a TextView to display 'Currently open' in green text.
+     *
+     * @param openIndicator The TextView to set to open.
+     */
+    private void setStatusOpen(TextView openIndicator) {
+        openIndicator.setText(R.string.currently_open);
+        openIndicator.setTextColor(getResources().getColor(R.color.holo_green_light));
+    }
+    //endregion
+
+    //region OnClick methods
+    @SuppressWarnings("unused")
+    @OnClick({R.id.btn_call_parish_office, R.id.btn_call_parish_office2, R.id.btn_call_ccf_office})
+    public void onClickCall(Button button) {
+        String phoneNumber = "tel:";
+        switch (button.getId()) {
+            case R.id.btn_call_parish_office:
+            case R.id.btn_call_parish_office2:
+                phoneNumber += getString(R.string.parish_office_phone);
+                break;
+            case R.id.btn_call_ccf_office:
+                phoneNumber += getString(R.string.ccf_office_phone);
+                break;
+        }
+
+        Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+        dialIntent.setData(Uri.parse(phoneNumber));
+        startActivity(dialIntent);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick({R.id.btn_email_parish_office, R.id.btn_email_ccf_office})
+    public void onClickEmail(Button button) {
+        String email = "mailto:";
+        switch (button.getId()) {
+            case R.id.btn_email_parish_office:
+                email += getString(R.string.parish_office_email);
+                break;
+            case R.id.btn_email_ccf_office:
+                email += getString(R.string.ccf_office_email);
+                break;
+        }
+
+        Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.parse(email));
+        startActivity(emailIntent);
+    }
+
+    @SuppressWarnings("unused")
+    @OnClick({R.id.btn_navigate_parish_office, R.id.btn_navigate_ccf_office})
+    public void onClickNavigate(Button button) {
+        String latitude, longitude;
+        switch (button.getId()) {
+            case R.id.btn_navigate_parish_office:
+                latitude = getString(R.string.parish_office_latitude);
+                longitude = getString(R.string.parish_office_longitude);
+                break;
+            case R.id.btn_navigate_ccf_office:
+                latitude = getString(R.string.ccf_office_latitude);
+                longitude = getString(R.string.ccf_office_longitude);
+                break;
+            default:
+                latitude = getString(R.string.parish_office_latitude);
+                longitude = getString(R.string.parish_office_longitude);
+        }
+        String mapsUri = String.format(getString(R.string.map_url), latitude, longitude);
+        Intent mapsIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(mapsUri));
+        startActivity(mapsIntent);
     }
     //endregion
 }
